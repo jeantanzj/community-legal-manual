3e4815c248bd06363cea8df26ecd856218eee43c
Jean <jean@data.gov.sg>
2017-11-30 13:44:13 +0800

    Support GET for checking if server can be connected to

--- app.js
+++ app.js
@@ -6,16 +6,28 @@ var request = require('request');
 var HTTP_PORT = process.env.PORT || 3000;
 var ENV = {'BONSAI_URL': process.env.BONSAI_URL || '' , 
 	'BONSAI_USERNAME': process.env.BONSAI_USERNAME || '', 
-	'BONSAI_PASSWORD': process.env.BONSAI_PASSWORD || '' };
+	'BONSAI_PASSWORD': process.env.BONSAI_PASSWORD || '',
+	'BONSAI_SEARCH_ENDPOINT' : process.env.BONSAI_SEARCH_ENDPOINT || ''};
+
 Object.values(ENV).forEach(function(val) { 
 	if(val === ''){
 		console.log('One or more environment variables for connecting to the elastic search database is missing: '+ JSON.stringify(ENV));
 		process.exit(1);
 	}
 });
+
+var allowCrossDomain = function(request, response, next) {
+    response.header('Access-Control-Allow-Origin', "*");
+    response.header('Access-Control-Allow-Methods', 'GET,POST');
+    response.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+} 
+
 var app = express();
+app.use(allowCrossDomain);
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+
 
 http.createServer(app)
 	.listen(HTTP_PORT, function() { console.log("Listening on port " + HTTP_PORT); });
@@ -27,7 +39,9 @@ app.post('/search', function(req, res){
 	var query = req.body.query;
 	esSearch(query, res);
 });
-
+app.get('/search', function(req, res){
+	res.send('Endpoint: POST /search');
+});
 btoa = function(str) { return Buffer.from(str).toString('base64'); };
 
 createEsQuery = function(queryStr) {
@@ -54,19 +68,20 @@ createEsQuery = function(queryStr) {
   };
 
 esSearch = function(query, res) {
-    var auth, esQuery;
+    var auth, esQuery, esQueryJson, options;
    	auth = btoa(ENV.BONSAI_USERNAME+':'+ENV.BONSAI_PASSWORD);
     esQuery = createEsQuery(query);
-    var options = {
-		  url: ENV.BONSAI_URL,
+    esQueryJson = JSON.stringify(esQuery);
+    options = {
+		  url: ENV.BONSAI_URL+ENV.BONSAI_SEARCH_ENDPOINT,
 		  headers: {
 		    'Content-Type': 'application/json',
 		    'Authorization': 'Basic ' + auth
-		  }
+		  },
+		  body: esQueryJson
 	};
     return request.post(
 	    options,
-	    JSON.stringify(esQuery),
 	    function (error, response, body) {
 	    	retVal = {"server" : options.url};
 	        if (!error && response.statusCode == 200) {
